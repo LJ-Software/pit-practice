6f4be36f1222ca9cbeb8281860735d439c7b32cc
Adrian Rivera-Zayas <adrianriveraza@gmail.com>
2018-12-03 13:55:50 -0600

    Add all return type methods

--- src/main/java/org/adrlog/practice/App.java
+++ src/main/java/org/adrlog/practice/App.java
@@ -73,7 +73,7 @@ public class App {
         System.out.println("Hello, World");
     }
 
-    public void runThings(int a, long b, double c, float d){
+    public void runThings(int a, long b, double c, float d) {
         a = a + 1;
         a = a * 1;
         a = a - 1;
@@ -138,4 +138,52 @@ public class App {
         System.out.println("Hello, World");
     }
 
+    public void returnVoid() {
+        System.out.println("returnVoid() called");
+    }
+
+    public short returnShort() {
+        short s = 0;
+        return s;
+    }
+
+    public long returnLong() {
+        long l = 0L;
+        return l;
+    }
+
+    public int returnInt() {
+        int i = 0;
+        return i;
+    }
+
+    public String returnString() {
+        String s = "aa";
+        return s;
+    }
+
+    public double returnDouble() {
+        double d = 0.0d;
+        return d;
+    }
+
+    public char returnChar() {
+        char a = 'a';
+        return a;
+    }
+
+    public Object returnObject() {
+        Object obj = new Object();
+        return obj;
+    }
+
+    public float returnFloat() {
+        float f = 0f;
+        return f;
+    }
+
+    public byte returnByte() {
+        byte b = 68;
+        return b;
+    }
 }
